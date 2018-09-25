bb0c78a100773292aa0b89186e97ae84d5daed6c
Alexander Tsirel <noma4i@gmail.com>
2018-09-25 10:05:38 +1000

    Better script with connection handling and input checkups

--- unseal_script.rb
+++ unseal_script.rb
@@ -4,22 +4,36 @@ require 'date'
 $stdout.sync = true
 $stderr.sync = true
 
+def check_input
+  @unseal_key_array = ENV['UNSEAL_KEYS']&.split(' ') || []
+  abort "Not Valid Vault Token: #{key}" if @unseal_key_array.map{|k| k.bytesize != 44}.any?
+  abort 'RECHECK_TIME is not valid' unless ENV['RECHECK_TIME']&.to_i > 0
+  abort 'VAULT_ADDR is not valid' unless ENV['VAULT_ADDR']&.size > 0
+end
+
+def unseal_loop
+
+  loop do
+    if Vault.sys.seal_status.sealed?
+      @unseal_key_array.each do |key|
+        Vault.sys.unseal(key)
+      end
+
+      puts "Vault has been unsealed on #{DateTime.now.strftime('%D %T')}"
+    end
+    sleep ENV['RECHECK_TIME'].to_i
+  end
+rescue Vault::HTTPConnectionError
+  puts "VAULT at: #{ENV['VAULT_ADDR']} is UNREACHABLE on #{DateTime.now.strftime('%D %T')}"
+  sleep ENV['RECHECK_TIME'].to_i
+
+  unseal_loop
+end
+
+check_input
+
 puts 'Starting Sentenial'
 puts "Looking after VAULT at: #{ENV['VAULT_ADDR']}"
 puts "Will unseal every #{ENV['RECHECK_TIME']} seconds"
 
-
-unseal_key_array = ENV['UNSEAL_KEYS'].split(' ')
-
-loop do
-  if Vault.sys.seal_status.sealed?
-    unseal_key_array.each do |key|
-      abort "Not Valid Vault: #{key}" if key.bytesize != 44
-
-      Vault.sys.unseal(key)
-    end
-
-    puts "Vault has been unsealed on #{DateTime.now.strftime('%D %T')}"
-  end
-  sleep ENV['RECHECK_TIME'].to_i
-end
+unseal_loop
